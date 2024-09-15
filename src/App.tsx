a8d3be17140eeb7e4d6f7cbf06b8eab0d2a5fb93
jasonqiu212 <jason_qiu@hotmail.com>
2024-09-15 21:13:52 +0800

    fix: fix header padding and background color

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import Landing from './pages/Landing';
 function App() {
   return (
     <MantineProvider>
-      <AppShell header={{ height: 128 }}>
-        <AppShell.Header p="40px">
+      <AppShell header={{ height: 96 }}>
+        <AppShell.Header px="40px" py="24px" bg="gray.1">
           <a href=".">
             <img src={logo} className="logo" alt="Logo" />
           </a>
         </AppShell.Header>
-        <AppShell.Main h="calc(100vh - 128px)" w="100%">
+        <AppShell.Main h="calc(100vh - 96px)" w="100%">
           <Landing />
         </AppShell.Main>
       </AppShell>
