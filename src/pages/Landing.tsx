550cf0384c371ff4d6d77fc03c9b16b543df8af4
jasonqiu212 <jason_qiu@hotmail.com>
2024-09-15 22:53:10 +0800

    fix: fix landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import {
   ActionIcon,
+  Button,
   Center,
-  Space,
   Stack,
   Text,
   Textarea,
@@ -12,27 +12,36 @@ import { IconArrowNarrowRight } from '@tabler/icons-react';
 
 function Landing() {
   const rightArrowActionButton = (
-    <ActionIcon variant="subtle" c="indigo.6">
+    <ActionIcon variant="filled" c="gray.4" bg="gray.1">
       <IconArrowNarrowRight />
     </ActionIcon>
   );
 
   return (
     <Center h="100%">
-      <Stack maw="720px" justify="center" ta="center" gap="16px">
-        <Title order={1} c="gray.9">
-          Your solution building companion
-        </Title>
-        <Text size="lg" c="gray.7">
-          Let's start with the general solution you're looking at.
-        </Text>
-        <Space h="16px" />
-        <Textarea
-          variant="filled"
-          autosize
-          placeholder="What do you wish to build?"
-          rightSection={rightArrowActionButton}
-        />
+      <Stack gap="40px">
+        <Stack maw="720px" justify="center" ta="center" gap="24px">
+          <Title order={1} c="gray.9">
+            Your solution building companion
+          </Title>
+          <Text size="lg" c="gray.7">
+            Let's begin by understanding a little more about what you want to
+            solve.
+          </Text>
+        </Stack>
+        <Stack align="center" gap="24px">
+          <Textarea
+            variant="filled"
+            w="100%"
+            autosize
+            maxRows={9}
+            placeholder="You can start by providing some context to your problem."
+            rightSection={rightArrowActionButton}
+          />
+          <Button variant="light" c="indigo.6" fw="400" bg="indigo.0">
+            I have a solution in mind
+          </Button>
+        </Stack>
       </Stack>
     </Center>
   );
