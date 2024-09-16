f52be4cb54941927e5a7c5cc222b90cfa77fe6d7
Zsigmond <e0725064@u.nus.edu>
2024-09-17 02:31:31 +0800

    set whole of Landing page to have max width of 720px

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,21 +1,13 @@
 import { Button, Center, Stack, Text, Title } from '@mantine/core';
 import '@mantine/core/styles.css';
-import { useNavigate } from 'react-router-dom';
 
 import ChatInput from '../components/ChatInput';
 
 function Landing() {
-  const navigate = useNavigate();
-
-  const handleInputButtonClick = () => {
-    // TODO: Pass user's input to next page
-    navigate('problem');
-  };
-
   return (
     <Center h="100%">
-      <Stack gap="40px">
-        <Stack maw="720px" justify="center" ta="center" gap="24px">
+      <Stack gap="40px" maw="720px">
+        <Stack w="100%" justify="center" ta="center" gap="24px">
           <Title order={1} c="gray.9">
             Your solution building companion
           </Title>
@@ -28,7 +20,6 @@ function Landing() {
           <ChatInput
             placeholder="You can start by providing some context to your problem."
             backgroundColor="gray.0"
-            handleButtonClick={handleInputButtonClick}
           />
           <Button variant="light" c="indigo.6" fw="400" bg="indigo.0">
             I have a solution in mind
