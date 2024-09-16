a70219a0d155d2daad710015827cace70b33b3d0
Zsigmond <e0725064@u.nus.edu>
2024-09-17 02:32:04 +0800

    use AccessControlAgent in Landing page

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,58 +1,107 @@
-import { ActionIcon, Group, Textarea } from '@mantine/core';
+import {
+  ActionIcon,
+  Alert,
+  Group,
+  LoadingOverlay,
+  Modal,
+  Text,
+  Textarea,
+} from '@mantine/core';
 import '@mantine/core/styles.css';
+import { useDisclosure } from '@mantine/hooks';
 import { IconArrowNarrowRight } from '@tabler/icons-react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { AccessControlAgent } from '../agents/AccessControlAgent';
 
 interface ChatInputProps {
   placeholder?: string;
   backgroundColor?: string;
-  handleButtonClick: () => void;
 }
 
-function ChatInput({
-  placeholder,
-  backgroundColor,
-  handleButtonClick,
-}: ChatInputProps) {
+function ChatInput({ placeholder, backgroundColor }: ChatInputProps) {
   const [inputValue, setInputValue] = useState('');
+  const [isModalOpened, { open: setModalOpen, close: setModalClose }] =
+    useDisclosure(false);
+  const [isLoadingOpened, { open: setLoadingOpen, close: setLoadingClose }] =
+    useDisclosure(false);
+  const [accessControlAgentMessage, setAccessControlAgentMessage] =
+    useState('');
+  const navigate = useNavigate();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue(event.target.value);
   };
 
+  const showAccessControlAgentMessage = (message: string) => {
+    setAccessControlAgentMessage(message);
+    setLoadingClose();
+    setModalOpen();
+  };
+
+  const accessControlAgent = new AccessControlAgent(
+    () => navigate('problem'),
+    showAccessControlAgentMessage,
+  );
+
+  const handleSubmit = () => {
+    accessControlAgent.invoke(inputValue);
+    setLoadingOpen();
+  };
+
   return (
-    <Group
-      w="100%"
-      gap="xs"
-      px="24px"
-      py="16px"
-      bg={backgroundColor}
-      bd="1.5px solid gray.4"
-      style={{ borderRadius: '4px' }}
-    >
-      <Textarea
-        value={inputValue}
-        onChange={handleInputChange}
-        style={{ flexGrow: 1 }}
-        variant="unstyled"
-        autosize
-        maxRows={9}
-        placeholder={placeholder}
-      />
-      <ActionIcon
-        disabled={inputValue ? false : true}
-        onClick={() => {
-          setInputValue('');
-          handleButtonClick();
-        }}
-        variant="filled"
-        size="sm"
-        c="gray.4"
-        bg={inputValue ? 'indigo.6' : 'gray.1'}
+    <>
+      <Group
+        w="100%"
+        gap="xs"
+        px="24px"
+        py="16px"
+        bg={backgroundColor}
+        bd="1.5px solid gray.4"
+        style={{ borderRadius: '4px' }}
       >
-        <IconArrowNarrowRight />
-      </ActionIcon>
-    </Group>
+        <Textarea
+          value={inputValue}
+          onChange={handleInputChange}
+          style={{ flexGrow: 1 }}
+          variant="unstyled"
+          autosize
+          maxRows={9}
+          placeholder={placeholder}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter' && !event.shiftKey) {
+              event.preventDefault();
+              handleSubmit();
+            }
+          }}
+        />
+        <ActionIcon
+          disabled={inputValue ? false : true}
+          onClick={() => {
+            handleSubmit();
+          }}
+          variant="filled"
+          size="sm"
+          c="gray.4"
+          bg={inputValue ? 'indigo.6' : 'gray.1'}
+        >
+          <IconArrowNarrowRight />
+        </ActionIcon>
+      </Group>
+      <Modal
+        centered
+        onClose={() => {
+          setInputValue('');
+          setModalClose();
+        }}
+        opened={isModalOpened}
+        title={<Text fw="bold">Unauthorised</Text>}
+      >
+        <Alert color="red">{accessControlAgentMessage}</Alert>
+      </Modal>
+      <LoadingOverlay visible={isLoadingOpened} />
+    </>
   );
 }
 
