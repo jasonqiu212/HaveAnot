20d3ed4fd07f15eb42158ca3d49ce0c9fdd97d9a
jasonqiu212 <jason_qiu@hotmail.com>
2024-09-17 23:02:30 +0800

    feat: add recommended products

--- src/components/RecommendedProducts.tsx
+++ src/components/RecommendedProducts.tsx
@@ -1,22 +1,74 @@
 import { Button, Stack, Text, Title } from '@mantine/core';
 import '@mantine/core/styles.css';
 
+import GatherSGLogo from '../assets/products/gather-sg.png';
+import PostmanLogo from '../assets/products/postman.png';
+import ProductCard from './Product';
+
 interface RecommendedProductsProps {
   nextStep: () => void;
   previousStep: () => void;
 }
 
+interface Product {
+  name: string;
+  description: string;
+  logoPath: string;
+  websiteLink: string;
+}
+
+const products: { [key: string]: Product } = {
+  GatherSG: {
+    name: 'GatherSG',
+    description:
+      'Lightweight Whole-of-Government customer relationship management system.',
+    logoPath: GatherSGLogo,
+    websiteLink: 'https://gather.gov.sg/',
+  },
+  Postman: {
+    name: 'Postman',
+    description: 'Secure, multichannel tool to send official communications.',
+    logoPath: PostmanLogo,
+    websiteLink: 'https://postman.gov.sg/',
+  },
+};
+
 function RecommendedProducts({
   nextStep,
   previousStep,
 }: RecommendedProductsProps) {
+  const recommendedProducts = ['GatherSG', 'Postman'];
+
   return (
-    <Stack justify="flex-start" gap="16px" c="gray.9">
-      <Title order={3}>Suggested products (2)</Title>
-      <Text size="md" c="gray.7">
-        Here's what we think would help address your problem.
-      </Text>
-      <Button
+    <Stack gap="40px">
+      <Stack gap="16px">
+        <Title order={3} c="gray.9">
+          Suggested products (2)
+        </Title>
+        <Text c="gray.7">
+          Here's what we think would help address your problem.
+        </Text>
+      </Stack>
+
+      <Stack gap="24px">
+        {recommendedProducts.map(
+          (recommendedProduct: string, index: number) => {
+            const { name, description, logoPath, websiteLink } =
+              products[recommendedProduct];
+            return (
+              <ProductCard
+                key={index}
+                name={name}
+                description={description}
+                logoPath={logoPath}
+                websiteLink={websiteLink}
+              />
+            );
+          },
+        )}
+      </Stack>
+
+      {/* <Button
         variant="light"
         fw="400"
         c="white"
@@ -24,7 +76,7 @@ function RecommendedProducts({
         onClick={nextStep}
       >
         What's next?
-      </Button>
+      </Button> */}
       <Button variant="outline" fw="400" c="indigo.6" onClick={previousStep}>
         Back
       </Button>
