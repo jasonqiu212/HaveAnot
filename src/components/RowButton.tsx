95c07a2437c28da38233603cd58a399a2f5a11e6
jasonqiu212 <jason_qiu@hotmail.com>
2024-09-17 21:01:10 +0800

    feat: add example questions

---
+++ src/components/RowButton.tsx
@@ -0,0 +1,26 @@
+import { Group, Text, ThemeIcon, UnstyledButton } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { IconChevronRight } from '@tabler/icons-react';
+
+interface RowButtonProps {
+  label: string;
+  handleButtonClick: () => void;
+}
+
+function RowButton({ label, handleButtonClick }: RowButtonProps) {
+  return (
+    <UnstyledButton onClick={handleButtonClick}>
+      <Group
+        p="16px"
+        style={{ borderBottom: '1px solid var(--mantine-color-gray-3)' }}
+      >
+        <Text c="gray.8" style={{ flexGrow: 1 }}>
+          {label}
+        </Text>
+        <IconChevronRight width="16px" color="var(--mantine-color-gray-8)" />
+      </Group>
+    </UnstyledButton>
+  );
+}
+
+export default RowButton;
