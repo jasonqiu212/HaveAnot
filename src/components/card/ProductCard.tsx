0cdc494a9fcf16205971e96e5fb4a6ae5ffb311e
jasonqiu212 <jason_qiu@hotmail.com>
2024-11-21 10:01:49 +0800

    fix: move logos to public

--- src/components/card/ProductCard.tsx
+++ src/components/card/ProductCard.tsx
@@ -1,7 +1,6 @@
 import { Group, Paper, Stack, Text, Title } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { IconArrowUpRight } from '@tabler/icons-react';
-import { useState } from 'react';
 
 interface ProductCardProps {
   name: string;
@@ -11,20 +10,12 @@ interface ProductCardProps {
 }
 
 function ProductCard({ name, description, websiteLink }: ProductCardProps) {
-  // TODO: Temporary fix
-  const [logo, setLogo] = useState<any>();
-
-  import(`../../assets/products/${name}.png`).then((image) => {
-    setLogo(image.default);
-  });
-
   return (
     <Paper p="24px" bg="indigo.0" radius="md">
       <Stack gap="16px">
         <a href={websiteLink} target="_blank">
           <Group>
-            {/* {logoPath && <img src={logoPath} alt={name} height="24px" />} */}
-            {logo && <img src={logo} alt={name} height="24px" />}
+            <img src={`/products/${name}.png`} alt={name} height="24px" />
             <Title order={4} c="gray.9" style={{ flexGrow: 1 }}>
               {name}
             </Title>
